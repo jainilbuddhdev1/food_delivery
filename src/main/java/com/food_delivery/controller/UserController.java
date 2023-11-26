a73b4ec697a8094610c6bd07f8fa4a78942ffa2c
jainilbuddhdev1 <jainilbuddhdev@gmail.com>
2023-11-26 17:29:32 +0530

    Removed CORS error

--- src/main/java/com/food_delivery/controller/UserController.java
+++ src/main/java/com/food_delivery/controller/UserController.java
@@ -3,12 +3,25 @@ package com.food_delivery.controller;
 import com.food_delivery.entity.UserEntity;
 import com.food_delivery.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(
+        origins = {
+                "http://localhost:4200"
+        },
+        methods = {
+                RequestMethod.OPTIONS,
+                RequestMethod.GET,
+                RequestMethod.PUT,
+                RequestMethod.DELETE,
+                RequestMethod.POST
+        })
 @RestController
 @RequestMapping("/user")
 public class UserController {
